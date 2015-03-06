e4308962cdcedf2cd576080004c492be380a9846
ssd12 <simrandhillon.ssd@gmail.com>
2015-03-06 16:32:49 -0500

    Second commit

--- mongodb_test.py
+++ mongodb_test.py
@@ -1,22 +1,16 @@
 import pymongo
 
 #mydb is independent database
-conn = pymongo.MongoClient('ec2-54-173-26-10.compute-1.amazonaws.com', 22, ssl=True, ssl_keyfile='simran.pem')
-db = conn.mydb
-db
+conn = pymongo.MongoClient();
 conn.database_names()
 
-collection = db.my_collection
-collection
-db.collection_names()
-
+collection = db.saveyour
 print('Going to create some new documentes');
 
 #create a simple document
 def newUser(login, usrname, pwd):
     doc = {"name": usrname, "login": login, "password":pwd}
     collection.insert(doc)
-
 
 newUser("ssd@saveyour.com", "ssd1", "pwd1");
 newUser("john@saveyour.com", "j1", "pwd2");
