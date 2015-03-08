4f00dfae4b7f989151daa16c00aed36071c168be
ssd12 <simrandhillon.ssd@gmail.com>
2015-03-08 15:03:13 -0400

    Added functions to fetchUserData.py

---
+++ fetchUserData.py
@@ -0,0 +1,28 @@
+import pymongo
+
+class fetchUserData:
+
+    def __init__(self, usrname, pwd):
+        self.u = usrname;
+        self.p = pwd;
+        conn = pymongo.mongoClient();
+        collection = db.SaveYourDB
+        print('Connecting to saveyour db')
+
+    def userExists(self.u):
+        result = collection.find_one({usr:self.u})
+        return result
+
+    def getPassword(self.u):
+        result  = collection.find_one({usr:self.u})
+        return result
+
+    def getData(self.u):
+        result  = collection.find_one({usr:self.u})
+        return result
+
+    def createNewUser(self.u, self.p):
+        newUser = {usr:self.u, pwd:self.p,data:""}
+        collection.insert(newUser)
+
+
