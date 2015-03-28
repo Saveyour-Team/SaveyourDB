a0c3ca4ee0bd54a81ad2b631409adb18f40f7c6d
johnn429 <jniland@umass.edu>
2015-03-28 15:47:21 -0400

    Deleted Names, Test
    Added init to setUserData

--- setUserData.py
+++ setUserData.py
@@ -1,3 +1,15 @@
 import pymongo
+from bson import Binary, Code
+from bson.json_util import dumps
+
 
 class setUserData:
+
+    def __init__(self, usrname, pwd):
+        self.u = usrname;
+        self.p = pwd;
+        conn = pymongo.mongoClient();
+        collection = db.SaveYourDB
+        print('Connecting to saveyour db')
+
+    
